aaf078e4980f668a180a6368fdbd81b25dc65d5f
Viraj Samarasinghe <123957527+virajsamarasinghe@users.noreply.github.com>
2025-02-11 00:22:07 +0530

    commite

---
+++ api/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose;
+
+// schema model
+const userSchema = new Schema({
+    name: String,
+    email: {
+        type: String,
+        trim: true,
+        minlength: 3
+    },
+    address: String,
+   
+    photoURL: String,
+    role: {
+        type: String,
+       enum: ['user', 'admin'],
+       default: 'user'
+    }
+})
+
+// create a model instance
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
